6b7f7e1dd4b5e0aee5e1ed21b158da766b630731
Karan kendre <karankendreg@gmail.com>
2025-03-20 19:55:29 +0530

    Fixed Deployment error

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
     {
+        path: 'audit/:id',
+        renderMode: RenderMode.Prerender,
+        getPrerenderParams: async () => {
+            // Generate params for all possible service IDs (0-4 based on the services array)
+            return Array.from({ length: 5 }, (_, i) => ({ id: i.toString() }));
+        }
+    },
+    {
         path: '**',
         renderMode: RenderMode.Prerender
     }
